01176238c8a150dec728029bce293acec7e1a156
pca006132 <john.lck40@gmail.com>
2016-06-11 15:26:31 +0800

    started simulation project. working on nbt parsing currently

---
+++ pcbSimulation/nbt/AbstractTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcbSimulation.nbt
+{
+    public abstract class AbstractTag
+    {
+        public readonly string name;
+        protected AbstractTag(string name)
+        {            
+            this.name = name;
+        }
+    }
+}
